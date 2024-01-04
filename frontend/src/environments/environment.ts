1d8da4a86893ffe8e052e963a4be4a4c13d24920
Robert Alex Barbu <robertalexbarbu@gmail.com>
2024-01-04 05:53:11 +0200

    Fix frontend environments

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,6 +3,6 @@ import { JsonObject } from 'type-fest';
 export const environment: JsonObject = {
   production: false,
   scheme: 'http',
-  domain: 'localhost',
+  domain: '157.230.108.218',
   port: 3000
 };
