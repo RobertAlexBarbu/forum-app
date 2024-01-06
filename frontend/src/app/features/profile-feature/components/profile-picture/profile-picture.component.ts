19f2ba3856c0fcacb11af2fc7f1020d010272ff9
Robert Alex Barbu <robertalexbarbu@gmail.com>
2024-01-06 17:02:40 +0200

    Fix file upload

--- frontend/src/app/features/profile-feature/components/profile-picture/profile-picture.component.ts
+++ frontend/src/app/features/profile-feature/components/profile-picture/profile-picture.component.ts
@@ -8,19 +8,22 @@ import {
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserModel } from '../../../../core/models/user.model';
-import { FileUploadHandlerEvent, FileUploadModule } from 'primeng/fileupload';
+import { FileUploadModule } from 'primeng/fileupload';
 import { Subject, takeUntil } from 'rxjs';
 import { ProfileService } from '../../services/profile/profile.service';
 import { AuthStateModel } from '../../../../core/models/auth-state.model';
 import { IsUserPipe } from '../../../../shared/pipes/is-user.pipe';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { jamCameraF } from '@ng-icons/jam-icons';
 
 @Component({
   selector: 'app-profile-picture',
   standalone: true,
-  imports: [CommonModule, FileUploadModule, IsUserPipe],
+  imports: [CommonModule, FileUploadModule, IsUserPipe, NgIcon],
   templateUrl: './profile-picture.component.html',
   styleUrls: ['./profile-picture.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  viewProviders: [provideIcons({ jamCameraF })]
 })
 export class ProfilePictureComponent implements OnInit, OnDestroy {
   @Input() user!: UserModel;
@@ -44,11 +47,18 @@ export class ProfilePictureComponent implements OnInit, OnDestroy {
       });
   }
 
-  onUpload(event: FileUploadHandlerEvent) {
-    for (const file of event.files) {
-      this.profileService.uploadProfilePicture(file, this.user.id).then(() => {
-        this.profilePictureUrl$.next(URL.createObjectURL(file));
-      });
+  onUpload(event: Event) {
+    const target = event.target;
+    if (target) {
+      const fileList = (target as HTMLInputElement).files;
+      if (fileList) {
+        const file = fileList[0];
+        this.profileService
+          .uploadProfilePicture(file, this.user.id)
+          .then(() => {
+            this.profilePictureUrl$.next(URL.createObjectURL(file));
+          });
+      }
     }
   }
 
